20dfed9a65f5f615deba5714b20602d57f20b2c8
Daria Slizkaya <Slizkaya.daria21@gmail.com>
2021-09-01 16:59:45 +0300

    task1

--- src/main/java/ru/netology/statistic/StatisticService.java
+++ src/main/java/ru/netology/statistic/StatisticService.java
@@ -1,4 +1,77 @@
 package ru.netology.statistic;
 
+
 public class StatisticService {
+
+    public long calculateSum(long[] sales) {
+
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+
+        }
+        return sum;
+    }
+
+    public long calculateAverageAmount(long[] sales) {
+
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        return sum / 12;
+    }
+
+    public long monthMax(long[] sales) {
+
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[maxMonth]) {
+                maxMonth = month;
+
+            }
+            month = month + 1;
+        }
+        return maxMonth;
+
+    }
+
+    public long monthMin(long[] sales) {
+
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    public long aboveAverageAmount(long[] sales) {
+        int averageAmount = 15;
+        int numbersOfMonth = 0;
+        for (long sale:sales){
+            if (sale < averageAmount){
+                numbersOfMonth = numbersOfMonth+1;
+            }
+        }
+
+        return numbersOfMonth;
+    }
+
+    public long overAverageAmount(long[] sales) {
+        int averageAmount = 15;
+        int numbersOfMonth = 0;
+        for (long sale:sales){
+            if (sale > averageAmount){
+                numbersOfMonth = numbersOfMonth+1;
+            }
+        }
+
+        return numbersOfMonth;
+    }
 }
+
--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -1,4 +1,72 @@
 package ru.netology.statistic;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StatisticServiceTest {
-}
+    @Test
+
+    void calculateSum(){
+        StatisticService service = new StatisticService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
+        long actual  = service.calculateSum(sales);
+
+        assertEquals(expected,actual);
+
+    }
+    @Test
+    void calculateAverageAmount(){
+        StatisticService service = new StatisticService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 15;
+        long actual = service.calculateAverageAmount(sales);
+
+        assertEquals(expected,actual);
+    }
+    @Test
+    void monthMax(){
+
+        StatisticService service = new StatisticService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8;
+        long actual = service.monthMax(sales);
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void monthMin(){
+
+        StatisticService service = new StatisticService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 9;
+        long actual = service.monthMin(sales);
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void aboveAverageAmount(){
+
+        StatisticService service = new StatisticService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+        long actual = service.aboveAverageAmount(sales);
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void overAverageAmount(){
+
+        StatisticService service = new StatisticService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+        long actual = service.aboveAverageAmount(sales);
+
+        assertEquals(expected,actual);
+    }
+    }
+
